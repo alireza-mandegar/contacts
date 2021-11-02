70be610cdb7e23c45acf659fefe26ae265d70f70
alireza-mandegar <alireza.mani95@gmail.com>
2021-11-02 23:05:35 +0330

    make get_email and the other functions based on name

--- main.py
+++ main.py
@@ -78,6 +78,73 @@ def get_number(name):
             print("[*] not found in contact! [*]")
 
 
+@click.command(help="to get user's email")
+@click.option("--name", help="this command is based on name")
+def get_email(name):
+    for row in cur.execute(F'SELECT * FROM contact ORDER BY name'):
+        if row[0] == name:
+            p = Person(row[0], row[1], row[2], row[3],
+                       row[4], row[5], row[6], row[7])
+            print(f"name is {p.name}\nemail is {p.email}")
+        else:
+            print("[*] not found in contact! [*]")
+
+@click.command(help="to get user's telegram")
+@click.option("--name", help="this command is based on name")
+def get_telegram(name):
+    for row in cur.execute(F'SELECT * FROM contact ORDER BY name'):
+        if row[0] == name:
+            p = Person(row[0], row[1], row[2], row[3],
+                       row[4], row[5], row[6], row[7])
+            print(f"name is {p.name}\ntelegram is {p.telegram} and the telegram link is https://t.me/{p.telegram}")
+        else:
+            print("[*] not found in contact! [*]")
+
+@click.command(help="to get user's instagram")
+@click.option("--name", help="this command is based on name")
+def get_instagram(name):
+    for row in cur.execute(F'SELECT * FROM contact ORDER BY name'):
+        if row[0] == name:
+            p = Person(row[0], row[1], row[2], row[3],
+                       row[4], row[5], row[6], row[7])
+            print(f"name is {p.name}\ninstagram is {p.instagram} and the instagram link is https://instagram.com/{p.instagram}")
+        else:
+            print("[*] not found in contact! [*]")
+
+@click.command(help="to get user's twitter")
+@click.option("--name", help="this command is based on name")
+def get_twitter(name):
+    for row in cur.execute(F'SELECT * FROM contact ORDER BY name'):
+        if row[0] == name:
+            p = Person(row[0], row[1], row[2], row[3],
+                       row[4], row[5], row[6], row[7])
+            print(f"name is {p.name}\ntwitter is {p.twitter} and the twitter link is https://twitter.com/{p.twitter}")
+        else:
+            print("[*] not found in contact! [*]")
+
+@click.command(help="to get user's github")
+@click.option("--name", help="this command is based on name")
+def get_github(name):
+    for row in cur.execute(F'SELECT * FROM contact ORDER BY name'):
+        if row[0] == name:
+            p = Person(row[0], row[1], row[2], row[3],
+                       row[4], row[5], row[6], row[7])
+            print(f"name is {p.name}\ngithub is {p.github} and the github link is https://github.com/{p.github}")
+        else:
+            print("[*] not found in contact! [*]")
+
+@click.command(help="to get user's linkedin")
+@click.option("--name", help="this command is based on name")
+def get_linkedin(name):
+    for row in cur.execute(F'SELECT * FROM contact ORDER BY name'):
+        if row[0] == name:
+            p = Person(row[0], row[1], row[2], row[3],
+                       row[4], row[5], row[6], row[7])
+            print(f"name is {p.name}\nlinkedin is {p.linkedin} and the linkedin link is https://linkedin.com/{p.linkedin}")
+        else:
+            print("[*] not found in contact! [*]")
+            
+
 group.add_command(get_user)
 group.add_command(add_user)
 if __name__ == "__main__":
