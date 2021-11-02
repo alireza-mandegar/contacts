2efada83e8a30adeda0ba85f508a498b1cd4d023
alireza-mandegar <alireza.mani95@gmail.com>
2021-11-02 14:12:53 +0330

    add get-user command

--- main.py
+++ main.py
@@ -8,59 +8,34 @@ def group():
 
 class Person:
     def __init__(self, name, number, email, telegram, instagram, twitter, github, linkedin):
-        self.name = name
-        self.number = number
-        self.email = email
-        self.telegram = telegram
-        self.instagram = instagram
-        self.twitter = twitter
-        self.github = github
-        self.linkedin = linkedin
-
-    def get_name(self):
-        return self.name
-
-    def get_number(self):
-        return self.number
-
-    def get_email(self):
-        return self.email
-
-    def get_telegram(self):
-        return self.telegram
-
-    def get_telegram_link(self):
-        return f"t.me/{self.telegram}"
-
-    def get_instagram(self):
-        return self.instagram
-
-    def get_instagram_link(self):
-        return f"instagram.com/{self.instagram}"
-
-    def get_twitter(self):
-        return self.twitter
-
-    def get_twitter_link(self):
-        return f"twitter.com/{self.twitter}"
-
-    def get_github(self):
-        return self.github
-
-    def get_github_link(self):
-        return f"github.com/{self.github}"
-
-    def get_linkedin(self):
-        return self.linkedin
-
-    def get_linkedin_link(self):
-        return f"linkedin.com/{self.linkedin}"
+        self.name: str = name
+        self.number: str = number
+        self.email: str = email
+        self.telegram: str = telegram
+        self.instagram: str = instagram
+        self.twitter: str = twitter
+        self.github: str = github
+        self.linkedin: str = linkedin
 
 
-def main():
-    p = Person("alireza", "09221235677", "alireza.mani95@gmail.com", "alireza_mandegar",
-               "alirezaamandegar", "alirzamandegar", "alireza-mandegar", "alirezamandegar")
+p = Person("alireza", "09221235677", "alireza.mani95@gmail.com", "alireza_mandegar",
+           "alirezaamandegar", "alirzamandegar", "alireza-mandegar", "alirezamandegar")
 
 
+@click.command()
+def get_user(p=p):
+    print(f"""
+                    name is {p.name}
+                    number is {p.number}
+                    email is {p.email}
+                    telegram is {p.telegram} and the link is https://t.me/{p.telegram}
+                    instagram is {p.instagram} and the link is https://instagram.com/{p.instagram}
+                    twitter is {p.twitter} and the link is https://twitter.com/{p.twitter}
+                    github is {p.github} and the link is https://github.com/{p.github}
+                    linkedin is {p.linkedin} and the link is https://linkedin.com/{p.linkedin}
+                """)
+
+
+group.add_command(get_user)
 if __name__ == "__main__":
-    pass
+    group()
