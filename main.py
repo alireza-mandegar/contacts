86a22641099b239f5c01fac8bd0272e8643dfc02
alireza-mandegar <alireza.mani95@gmail.com>
2021-11-03 00:19:10 +0330

    add all changing function of all att

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class Person:
         self.linkedin: str = linkedin
 
 
-# p = Person("alireza", "09221235677", "alireza.mani95@gmail.com", "alireza_mandegar",
+# p = Person("alireza", "09123456789", "alireza.mani95@gmail.com", "alireza_mandegar",
 #            "alirezaamandegar", "alirzamandegar", "alireza-mandegar", "alirezamandegar")
 
 con = sqlite3.connect("./contact.db")
@@ -27,7 +27,110 @@ cur = con.cursor()
 # cur.execute('''CREATE TABLE contact
 #                 (name, number, email, telegram, instagram, twitter, github, linkedin)''')
 
-# def change_user
+
+@click.command(help="to update number in your contact")
+@click.option("--name", help="this command is based on name")
+@click.argument("new_number")
+def change_number(name, new_number):
+    for row in cur.execute(F'SELECT * FROM contact ORDER BY name'):
+        if row[0] == name:
+            cur.execute(
+                f"UPDATE contact set number = '{new_number}' where name = '{name}'")
+            con.commit()
+            print(
+                f"[*] the contact has been updated! [*]\n[*] new number is {new_number} [*]]")
+            return
+    print("[*] not found in contact! [*]")
+
+
+@click.command(help="to update email in your contact")
+@click.option("--name", help="this command is based on name")
+@click.argument("new_email")
+def change_email(name, new_email):
+    for row in cur.execute(F'SELECT * FROM contact ORDER BY name'):
+        if row[0] == name:
+            cur.execute(
+                f"UPDATE contact set email = '{new_email}' where name = '{name}'")
+            con.commit()
+            print(
+                f"[*] the contact has been updated! [*]\n[*] new email is {new_email} [*]")
+            return
+    print("[*] not found in contact! [*]")
+
+
+@click.command(help="to update telegram in your contact")
+@click.option("--name", help="this command is based on name")
+@click.argument("new_telegram")
+def change_telegram(name, new_telegram):
+    for row in cur.execute(F'SELECT * FROM contact ORDER BY name'):
+        if row[0] == name:
+            cur.execute(
+                f"UPDATE contact set telegram = '{new_telegram}' where name = '{name}'")
+            con.commit()
+            print(
+                f"[*] the contact has been updated! [*]\n[*] new telegram is {new_telegram} [*]")
+            return
+    print("[*] not found in contact! [*]")
+
+
+@click.command(help="to update instagram in your contact")
+@click.option("--name", help="this command is based on name")
+@click.argument("new_instagram")
+def change_instagram(name, new_instagram):
+    for row in cur.execute(F'SELECT * FROM contact ORDER BY name'):
+        if row[0] == name:
+            cur.execute(
+                f"UPDATE contact set instagram = '{new_instagram}' where name = '{name}'")
+            con.commit()
+            print(
+                f"[*] the contact has been updated! [*]\n[*] new instagram is {new_instagram} [*]")
+            return
+    print("[*] not found in contact! [*]")
+
+
+@click.command(help="to update twitter in your contact")
+@click.option("--name", help="this command is based on name")
+@click.argument("new_twitter")
+def change_twitter(name, new_twitter):
+    for row in cur.execute(F'SELECT * FROM contact ORDER BY name'):
+        if row[0] == name:
+            cur.execute(
+                f"UPDATE contact set twitter = '{new_twitter}' where name = '{name}'")
+            con.commit()
+            print(
+                f"[*] the contact has been updated! [*]\n[*] new twitter is {new_twitter} [*]")
+            return
+    print("[*] not found in contact! [*]")
+
+
+@click.command(help="to update github in your contact")
+@click.option("--name", help="this command is based on name")
+@click.argument("new_github")
+def change_github(name, new_github):
+    for row in cur.execute(F'SELECT * FROM contact ORDER BY name'):
+        if row[0] == name:
+            cur.execute(
+                f"UPDATE contact set github = '{new_github}' where name = '{name}'")
+            con.commit()
+            print(
+                f"[*] the contact has been updated! [*]\n[*] new github is {new_github} [*]")
+            return
+    print("[*] not found in contact! [*]")
+
+
+@click.command(help="to update linkedin in your contact")
+@click.option("--name", help="this command is based on name")
+@click.argument("new_linkedin")
+def change_linkedin(name, new_linkedin):
+    for row in cur.execute(F'SELECT * FROM contact ORDER BY name'):
+        if row[0] == name:
+            cur.execute(
+                f"UPDATE contact set linkedin = '{new_linkedin}' where name = '{name}'")
+            con.commit()
+            print(
+                f"[*] the contact has been updated! [*]\n[*] new linkedin is {new_linkedin} [*]")
+            return
+    print("[*] not found in contact! [*]")
 
 
 @click.command(help="to add a user to your contact")
@@ -48,7 +151,7 @@ def add_user(name, number, email, telegram, instagram, twitter, github, linkedin
 @click.command(help="to get user completely")
 @click.option("--name", help="this command is based on name")
 def get_user(name):
-    for row in cur.execute(F'SELECT * FROM contact ORDER BY name'):
+    for row in cur.execute(f'SELECT * FROM contact ORDER BY name'):
         if row[0] == name:
             p = Person(row[0], row[1], row[2], row[3],
                        row[4], row[5], row[6], row[7])
@@ -62,8 +165,8 @@ twitter is {p.twitter} and the link is https://twitter.com/{p.twitter}
 github is {p.github} and the link is https://github.com/{p.github}
 linkedin is {p.linkedin} and the link is https://linkedin.com/{p.linkedin}
                     """)
-        else:
-            print("[*] not found in contact! [*]")
+            return
+    print("[*] not found in contact! [*]")
 
 
 @click.command(help="to get user's number")
@@ -74,8 +177,8 @@ def get_number(name):
             p = Person(row[0], row[1], row[2], row[3],
                        row[4], row[5], row[6], row[7])
             print(f"name is {p.name}\nnumber is {p.number}")
-        else:
-            print("[*] not found in contact! [*]")
+            return
+    print("[*] not found in contact! [*]")
 
 
 @click.command(help="to get user's email")
@@ -86,8 +189,9 @@ def get_email(name):
             p = Person(row[0], row[1], row[2], row[3],
                        row[4], row[5], row[6], row[7])
             print(f"name is {p.name}\nemail is {p.email}")
-        else:
-            print("[*] not found in contact! [*]")
+            return
+    print("[*] not found in contact! [*]")
+
 
 @click.command(help="to get user's telegram")
 @click.option("--name", help="this command is based on name")
@@ -96,9 +200,11 @@ def get_telegram(name):
         if row[0] == name:
             p = Person(row[0], row[1], row[2], row[3],
                        row[4], row[5], row[6], row[7])
-            print(f"name is {p.name}\ntelegram is {p.telegram} and the telegram link is https://t.me/{p.telegram}")
-        else:
-            print("[*] not found in contact! [*]")
+            print(
+                f"name is {p.name}\ntelegram is {p.telegram} and the telegram link is https://t.me/{p.telegram}")
+            return
+    print("[*] not found in contact! [*]")
+
 
 @click.command(help="to get user's instagram")
 @click.option("--name", help="this command is based on name")
@@ -107,9 +213,11 @@ def get_instagram(name):
         if row[0] == name:
             p = Person(row[0], row[1], row[2], row[3],
                        row[4], row[5], row[6], row[7])
-            print(f"name is {p.name}\ninstagram is {p.instagram} and the instagram link is https://instagram.com/{p.instagram}")
-        else:
-            print("[*] not found in contact! [*]")
+            print(
+                f"name is {p.name}\ninstagram is {p.instagram} and the instagram link is https://instagram.com/{p.instagram}")
+            return
+    print("[*] not found in contact! [*]")
+
 
 @click.command(help="to get user's twitter")
 @click.option("--name", help="this command is based on name")
@@ -118,9 +226,11 @@ def get_twitter(name):
         if row[0] == name:
             p = Person(row[0], row[1], row[2], row[3],
                        row[4], row[5], row[6], row[7])
-            print(f"name is {p.name}\ntwitter is {p.twitter} and the twitter link is https://twitter.com/{p.twitter}")
-        else:
-            print("[*] not found in contact! [*]")
+            print(
+                f"name is {p.name}\ntwitter is {p.twitter} and the twitter link is https://twitter.com/{p.twitter}")
+            return
+    print("[*] not found in contact! [*]")
+
 
 @click.command(help="to get user's github")
 @click.option("--name", help="this command is based on name")
@@ -129,9 +239,11 @@ def get_github(name):
         if row[0] == name:
             p = Person(row[0], row[1], row[2], row[3],
                        row[4], row[5], row[6], row[7])
-            print(f"name is {p.name}\ngithub is {p.github} and the github link is https://github.com/{p.github}")
-        else:
-            print("[*] not found in contact! [*]")
+            print(
+                f"name is {p.name}\ngithub is {p.github} and the github link is https://github.com/{p.github}")
+            return
+    print("[*] not found in contact! [*]")
+
 
 @click.command(help="to get user's linkedin")
 @click.option("--name", help="this command is based on name")
@@ -140,10 +252,11 @@ def get_linkedin(name):
         if row[0] == name:
             p = Person(row[0], row[1], row[2], row[3],
                        row[4], row[5], row[6], row[7])
-            print(f"name is {p.name}\nlinkedin is {p.linkedin} and the linkedin link is https://linkedin.com/{p.linkedin}")
-        else:
-            print("[*] not found in contact! [*]")
-            
+            print(
+                f"name is {p.name}\nlinkedin is {p.linkedin} and the linkedin link is https://linkedin.com/{p.linkedin}")
+            return
+    print("[*] not found in contact! [*]")
+
 
 group.add_command(get_user)
 group.add_command(add_user)
@@ -154,5 +267,12 @@ group.add_command(get_instagram)
 group.add_command(get_twitter)
 group.add_command(get_github)
 group.add_command(get_linkedin)
+group.add_command(change_number)
+group.add_command(change_email)
+group.add_command(change_telegram)
+group.add_command(change_instagram)
+group.add_command(change_twitter)
+group.add_command(change_github)
+group.add_command(change_linkedin)
 if __name__ == "__main__":
     group()
